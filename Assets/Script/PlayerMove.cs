a76a65c45298aa1a552623d0d9cab89798c4fff4
KevS003 <kjscordero@gmail.com>
2023-01-30 19:33:17 -0500

    Updates to the code
    I updated some code and also made boundaries to match Touhou.

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -9,12 +9,15 @@ using UnityEngine.Sprites;
 public class PlayerMove : MonoBehaviour
 {
     Animator animator;
-    public int maxHealth= 10000;
+    static public int lvlNum= 1;
+    public int maxHealth= 3;
     public int alreadyMult = 0;
     public float speed = 4.0f;
     public float bulletSpeed = 5000.0f;
     public float originalBspeed = 5000.0f;
     public float shotsPerS = 20.0f;
+    public float lvlTimer = 30.0f;
+    public int score = 0;
     float firedRound;
     int cHealth;
     public int health {get{return cHealth;}}//in case health needs to be read
@@ -46,6 +49,14 @@ public class PlayerMove : MonoBehaviour
         verti = Input.GetAxis("Vertical");
         hori = Input.GetAxis("Horizontal");
         Vector2 move = new Vector2(hori, verti);
+        if(lvlTimer >= 0.0f)
+        {
+            lvlTimer-=Time.deltaTime; 
+        }
+        else
+        {
+            //send to next level if alive.
+        }
         if(Input.GetKeyDown(KeyCode.R))
         {
             if(weaponType<3)
@@ -64,13 +75,14 @@ public class PlayerMove : MonoBehaviour
             gun(weaponType);
         }
     }
-     void FixedUpdate()
+    void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
         position.x = position.x + speed * hori * Time.deltaTime;//seconds to render a frame to keep movement consistent
         position.y = position.y + speed * verti * Time.deltaTime;
         rigidbody2d.MovePosition(position);
     }
+    //RETURN VALUE FROM WEAPON SCRIPT FOR SCORE!!!!!
     void gun(int weaponType)
     {
         
@@ -119,12 +131,5 @@ public class PlayerMove : MonoBehaviour
                 projectile.Launch(bulletSpeed);  
             } 
         }
-        
-        /*
-        GameObject projectileObject = Instantiate(projectileLaunch, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);//NO ROTATION
-        WeaponFire projectile = projectileObject.GetComponent<WeaponFire>();
-        projectile.AmmoType(weaponType);
-        projectile.Launch(bulletSpeed);
-        */
     }
 }
